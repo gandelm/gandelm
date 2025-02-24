5e820ec86d91e0782b3c37968baab141319104c2
kotaro.matsuya <02milkey15@gmail.com>
2025-02-24 23:57:32 +0900

    wip: github-webhook

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,8 +5,10 @@ import (
 	"net/http"
 
 	"github.com/gandelm/gandelm/generated/protocol/catalog/v1/catalogv1connect"
+	"github.com/gandelm/gandelm/generated/protocol/github_webhook/v1/github_webhookv1connect"
 	"github.com/gandelm/gandelm/internal/container"
 	"github.com/gandelm/gandelm/internal/service/catalog"
+	"github.com/gandelm/gandelm/internal/service/github_webhook"
 	"github.com/rs/cors"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
@@ -17,6 +19,7 @@ func Start(container container.Containerer) error {
 
 	mux := http.NewServeMux()
 	mux.Handle(catalogv1connect.NewCatalogServiceHandler(catalog.NewCatalogService(container)))
+	mux.Handle(github_webhookv1connect.NewGitHubWebhookServiceHandler(github_webhook.NewGithubWebhookService(container)))
 
 	corsHandler := cors.AllowAll().Handler(h2c.NewHandler(mux, &http2.Server{}))
 
